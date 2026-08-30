00e70aa1f2566dd39846c81baa4e83ead2444c11
Prashant Kshirsagar <134128560+prashantkshirsagar-git@users.noreply.github.com>
2026-08-30 09:02:21 +0530

    feat: add AiTools component

---
+++ client/src/components/AiTools.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AiTools = () => {
+  return (
+    <div className='px-4 sm:px-20 xl:px-32 my-24'>
+      <div className='text-center'>
+        <h2>Powerful AI Tools</h2>
+        <p>Everything you need to create, enhance and optimize your content with cutting-edge AI technology.</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default AiTools
