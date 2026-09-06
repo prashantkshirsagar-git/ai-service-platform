a8ca548aa6ee6da24cd4fb5c44f12873cf739529
Prashant Kshirsagar <134128560+prashantkshirsagar-git@users.noreply.github.com>
2026-09-06 12:36:31 +0530

    feat: add CreationItem component
    - Display creation prompt, type, and formatted date
    - Toggle expanded view on click to show full content
    - Render image content or markdown text depending on type

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { dummyCreationData } from "../assets/assets";
 import { Gem, Sparkles } from "lucide-react";
 import { Protect } from "@clerk/clerk-react";
+import CreationItem from "../components/CreationItem";
 
 const Dashboard = () => {
   const [creations, setCreations] = useState([]);
@@ -31,19 +32,23 @@ const Dashboard = () => {
           <div className="text-slate-600">
             <p className="text-sm">Active Plan</p>
             <h2 className="text-xl font-semibold">
-              
-               <Protect plan='premium' fallback="Free">Premium</Protect>
+              <Protect plan="premium" fallback="Free">
+                Premium
+              </Protect>
             </h2>
           </div>
-          <div className="w-10 h-10 rounded-lg bg-linear-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-centre items-center">
+          <div className="w-10 h-10 rounded-lg bg-linear-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center">
             <Gem className="w-5 text-white" />
           </div>
         </div>
       </div>
 
-<div className="space-y-3">
-<p className="mt-6 mb-4">Recent Creations</p>
-</div>
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {
+          creations.map((item)=> <CreationItem key={item.id} item={item}/>)
+        }
+      </div>
 
 
     </div>
